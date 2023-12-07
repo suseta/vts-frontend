15397c96c890ec936db55473a396a190290c708c
Rajdeep-krypc <rajdeep.r@krypc.com>
2023-12-07 23:02:50 +0530

    driver details api

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const {index,addVehicle,vehicleDetail,addDriver,driverDetail} =require("../controllers/controllerFunction")
+const {index,addDriver} =require("../controllers/controllerFunction")
 router.route("/").get(index);
 
-router.route("/addVehicle").post(addVehicle);
-router.route("/vehicleDetail").post(vehicleDetail);
+// router.route("/addVehicle").post(addVehicle);
+// router.route("/vehicleDetail").post(vehicleDetail);
 router.route("/addDriver").post(addDriver);
-router.route("/DriverDetail").post(driverDetail);
+// router.route("/DriverDetail").post(driverDetail);
 
 
 module.exports = router;
