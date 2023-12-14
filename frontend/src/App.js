a257dbc0be31bff67b859fafb74c3b6748e1fb59
suseta <suseta.datta@gmail.com>
2023-12-14 10:02:31 +0530

    vehicle form

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import VehicleForm from './Components/vehicle/vehicleForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Navitronicx</h1>
+      <h2>ADD Vehicle Into the Platform</h2>
+      <VehicleForm />
+    </div>
+  );
+}
+
+export default App;
