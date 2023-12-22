d6654d4bb682f1521858e82864c3a1b26e21f398
Suseta Datta <suseta.datta@gmail.com>
2023-12-22 15:00:47 +0530

    Driver Entry page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 // src/App.js
 import React from 'react'
 // import AssetRegistrationForm from './Components/assetDetails/assetRegistration'
-import AssetInfoForm from './Components/entityAssetInfo/entityAssetInfo'
+// import AssetInfoForm from './Components/entityAssetInfo/entityAssetInfo'
+import DriverEntryForm from './Components/driverDetails/driverEntry'
 
 function App () {
   return (
     <div className='App'>
   {/*<AssetRegistrationForm/>*/}
-  <AssetInfoForm/>
+  {/*<AssetInfoForm/>*/}
+  <DriverEntryForm/>
     </div>
   )
 }
