870b7299bc155d0bb16e7f98ed126814829acc61
Suseta Datta <suseta.datta@gmail.com>
2023-12-28 15:57:16 +0530

    App.js updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 // src/App.js
 import React from 'react'
+import DeviceDetailsEntryForm from './Components/deviceDetails/deviceDetails'
+// import PlantLocEntryInfoForm from './Components/plantLocationEntryInfo/plantLocInfo'
+// import TransporterEntryInfoForm from './Components/transporterEntryInfo/transporterInfo'
 // import AssetRegistrationForm from './Components/assetDetails/assetRegistration'
 // import AssetInfoForm from './Components/entityAssetInfo/entityAssetInfo'
 // import EntityEntryInfoForm from './Components/entityEntryInfo/entityInfo'
-import AssetEntryInfoForm from './Components/assetEntryInfo/assetInfo'
+// import AssetEntryInfoForm from './Components/assetEntryInfo/assetInfo'
+// import ConsigneeEntryInfoForm from './Components/consigneeEntryInfo/consigneeInfo'
+// import ConsignorEntryInfoForm from './Components/consignorEntryInfo copy/consignorInfo'
 
 function App () {
   return (
@@ -11,7 +16,12 @@ function App () {
       {/*<AssetRegistrationForm/>*/}
       {/*<EntityEntryInfoForm />*/}
       {/*<DriverEntryForm/>*/}
-      <AssetEntryInfoForm/>
+      {/*<AssetEntryInfoForm/>*/}
+      {/*<ConsigneeEntryInfoForm/>*/}
+      {/*<ConsignorEntryInfoForm/>*/}
+      {/*<TransporterEntryInfoForm/>*/}
+      {/*<PlantLocEntryInfoForm/>*/}
+      <DeviceDetailsEntryForm/>
     </div>
   )
 }
