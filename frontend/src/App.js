f117180857e45babc09636e32b25244cc6a15aa6
Rajdeep-krypc <rajdeep.r@krypc.com>
2024-01-12 17:14:38 +0530

    device Entry Form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,26 +2,27 @@
 import React from 'react'
 // import AssetRegistrationForm from './Components/assetRegistration/assetRegistration'
 // import TransporterCreationForm from './Components/transporterCreation/transporterCreation'
-// import DeviceEntryForm from './Components/deviceEntry/deviceEntry'
+import DeviceEntryForm from './Components/deviceEntry/deviceEntry'
 // import PlantLocEntryInfoForm from './Components/plantLocationEntryInfo/plantLocInfo'
 // import AssetInfoForm from './Components/entityAssetInfo/entityAssetInfo'
-import EntityCreationForm from './Components/entityCreation/entityCreation'
-// import AssetEntryInfoForm from './Components/assetEntryInfo/assetInfo'
+// import EntityCreationForm from './Components/entityCreation/entityCreation'
+// import AssetEntryInfoForm from './Components/assetEntryInfo/assetEntryInfo'
 // import ConsigneeEntryInfoForm from './Components/consigneeEntryInfo/consigneeInfo'
 // import ConsignorEntryInfoForm from './Components/consignorEntryInfo copy/consignorInfo'
+// import DriverEntryForm from './Components/driverDetails/driverEntry'
 
 function App () {
   return (
     <div className='App'>
       {/*<AssetRegistrationForm />*/}
-      <EntityCreationForm />
-      {/*<DriverEntryForm/>*/}
-      {/*<AssetEntryInfoForm/>*/}
+      {/* <EntityCreationForm /> */}
+      {/* <DriverEntryForm/> */}
+      {/* <AssetEntryInfoForm/> */}
       {/*<ConsigneeEntryInfoForm/>*/}
       {/*<ConsignorEntryInfoForm/>*/}
-      {/*<TransporterCreationForm />*/}
-      {/*<PlantLocEntryInfoForm/>*/}
-      {/*<DeviceEntryForm/>*/}
+      {/* <TransporterCreationForm /> */}
+      {/* <PlantLocEntryInfoForm/> */}
+      <DeviceEntryForm/>
     </div>
   )
 }
